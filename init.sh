992e49fcbae6a5c06d1eca2ef16c2e031a34f54e
Davod <davidkingnt@gmail.com>
2019-05-29 00:30:05 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,35 +9,45 @@ if [ -e "/config/gdrive-ocamlfuse" ]; then
   echo "No Google Drive configuration found."
 else
   if [ -z "${CLIENT_ID}" ]; then
-    echo "no CLIENT_ID found -> EXIT"
+    echo "Fatal: no CLIENT_ID found -> EXIT"
     exit 1
   elif [ -z "${CLIENT_SECRET}" ]; then
-    echo "no CLIENT_SECRET found -> EXIT"
+    echo "Fatal: no CLIENT_SECRET found -> EXIT"
     exit 1
   elif [ -z "$VERIFICATION_CODE" ]; then
-    echo "no VERIFICATION_CODE found -> EXIT"
+    echo "Warning: no VERIFICATION_CODE found"
+    setup()
     exit 1
   else
-    echo "Initialising google-drive-ocamlfuse..."
-    echo "${VERIFICATION_CODE}" | \
-      google-drive-ocamlfuse \
-        -label ${LABEL} \
-        -f \
-        -cc \
-        -skiptrash \
-        -headless \
-        -id "${CLIENT_ID}.apps.googleusercontent.com" \
-        -secret "${CLIENT_SECRET}" \
-        -config /config/gdrive-ocamlfuse
+    setup()
   fi
 fi
 
-echo "Mounting at ${DRIVE_PATH}"
-google-drive-ocamlfuse ${DRIVE_PATH} \
-  -o noatime,uid=${PUID},gid=${PGID},allow_other \
-  -label ${LABEL} \
-  -f \
-  -cc \
-  -skiptrash \
-  -headless \
-  -config /config/gdrive-ocamlfuse
+mount()
+
+# Functions
+setup() {
+  echo "Initialising google-drive-ocamlfuse..."
+  echo "${VERIFICATION_CODE}" | \
+    google-drive-ocamlfuse \
+      -label ${LABEL} \
+      -f \
+      -cc \
+      -skiptrash \
+      -headless \
+      -id "${CLIENT_ID}.apps.googleusercontent.com" \
+      -secret "${CLIENT_SECRET}" \
+      -config /config/gdrive-ocamlfuse
+}
+
+mount() {
+  echo "Mounting at ${DRIVE_PATH}"
+  google-drive-ocamlfuse ${DRIVE_PATH} \
+    -o noatime,uid=${PUID},gid=${PGID},allow_other \
+    -label ${LABEL} \
+    -f \
+    -cc \
+    -skiptrash \
+    -headless \
+    -config /config/gdrive-ocamlfuse
+}
